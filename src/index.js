e163229ab26ec4022fa1d5600b7062213a755a90
AndreeaSichitiu <sichitiuandreea@gmail.com>
2023-07-11 16:50:11 +0300

    update index 6

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import './styles.css';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import { smoothScroll } from './js/smooth_scroll';
 
-const searchForm = document.querySelector('.search-form');
+const searchForm = document.querySelector('#search-form');
 const gallery = document.querySelector('.gallery');
 const loadMoreBtn = document.querySelector('.load-more');
 
