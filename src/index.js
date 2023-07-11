655db472eede9e73b4501c29a6846c1e53653600
AndreeaSichitiu <sichitiuandreea@gmail.com>
2023-07-11 09:22:59 +0300

    update index

--- src/index.js
+++ src/index.js
@@ -1,17 +1,12 @@
-// markup html si css
-// api
-// importuri
-// select all la tot ce am nevoie
-// creare markup carduri
 
-import SimpleLightbox from "simplelightbox";
-import "simplelightbox/dist/simple-lightbox.min.css";
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 import { fetchImages } from './js/api';
 import { createMarkup } from './js/gallery_markup';
 import './styles.css';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import { smoothScroll } from './js/smooth_scroll';
- 
+
 searchForm = document.querySelector('.search-form');
 gallery = document.querySelector('.gallery');
 loadMoreBtn = document.querySelector('.load-more');
@@ -20,88 +15,82 @@ const perPage = 40;
 let page = 1;
 let searchPhoto = '';
 
-loadMoreBtn.classList.replace("load-more", "loader")
+loadMoreBtn.classList.replace('load-more', 'loader');
 loadMoreBtn.innerHTML = null;
 loadMoreBtn.classList.add('is-hidden');
 
-searchForm.addEventListener('submit', onSubmitForm);
+searchForm.addEventListener('submit', submitForm);
 
-function onSubmitForm(event) {
-    event.preventDefault();
+function submitForm(event) {
+  event.preventDefault();
 
-    gallery.innerHTML = '';
-    page = 1;
-    const { searchQuery } = event.currentTarget.elements;
-    searchPhoto = searchQuery.value
-        .trim()
-        .toLowerCase()
-        .split(' ')
-        .join('+');
-    
-    if (searchPhoto === '') {
-      
-        Notify.info('Enter your request, please!', {
-          position: 'center-center',
-        });
-        clearAll()
-        return;
-    }
+  gallery.innerHTML = '';
+  page = 1;
+  const { searchQuery } = event.currentTarget.elements;
+  searchPhoto = searchQuery.value.trim().toLowerCase().split(' ').join('+');
 
-    fetchImages(searchPhoto, page, perPage)
-        .then(data => {
-            const searchResults = data.hits;
-            if (data.totalHits === 0) {
-                Notify.failure('Sorry, there are no images matching your search query. Please try again.', {
-                  position: 'center-center',
-                });
-            } else {
-                Notify.info(`Hooray! We found ${data.totalHits} images.`);
-               createMarkup(searchResults);
-                lightbox.refresh();
+  if (searchPhoto === '') {
+    Notify.info('Enter your request, please!', {
+      position: 'center-center',
+    });
+    clearAll();
+    return;
+  }
 
-            };
-            if (data.totalHits > perPage) {
-              loadMoreBtn.classList.remove('is-hidden');
-                window.addEventListener('scroll', showLoadMorePage);
-            };
-            smoothScroll();
-        })
-        .catch(onError);
+  fetchImages(searchPhoto, page, perPage)
+    .then(data => {
+      const searchResults = data.hits;
+      if (data.totalHits === 0) {
+        Notify.failure(
+          'Sorry, there are no images matching your search query. Please try again.',
+          {
+            position: 'center-center',
+          }
+        );
+      } else {
+        Notify.info(`Hooray! We found ${data.totalHits} images.`);
+        createMarkup(searchResults);
+        lightbox.refresh();
+      }
+      if (data.totalHits > perPage) {
+        loadMoreBtn.classList.remove('is-hidden');
+        window.addEventListener('scroll', loadMorePage);
+      }
+      smoothScroll();
+    })
+    .catch(onError);
 
-        // loadMoreBtn.addEventListener('click', onClickLoadMore);
-
-    event.currentTarget.reset();
-};
+   event.currentTarget.reset();
+}
 
 function onClickLoadMore() {
-    page += 1;
-    fetchImages(searchPhoto, page, perPage)
-        .then(data => {
-            const searchResults = data.hits;
-            const numberOfPage = Math.ceil(data.totalHits / perPage);
-            
-            createMarkup(searchResults);
-            if (page === numberOfPage) {
-              loadMoreBtn.classList.add('is-hidden');
-                Notify.info("We're sorry, but you've reached the end of search results.");
-                // loadMoreBtn.removeEventListener('click', onClickLoadMore);
-                window.removeEventListener('scroll', showLoadMorePage);
-            };
-            lightbox.refresh();
-            smoothScroll();
-        })
-        .catch(onError);
-};
+  page += 1;
+  fetchImages(searchPhoto, page, perPage)
+    .then(data => {
+      const searchResults = data.hits;
+      const numberOfPage = Math.ceil(data.totalHits / perPage);
 
- 
+      createMarkup(searchResults);
+      if (page === numberOfPage) {
+        loadMoreBtn.classList.add('is-hidden');
+        Notify.info(
+          "We're sorry, but you've reached the end of search results."
+        );
+        window.removeEventListener('scroll', loadMorePage);
+      }
+      lightbox.refresh();
+      smoothScroll();
+    })
+    .catch(onError);
+}
 
-function showLoadMorePage() {
-    if (checkIfEndOfPage()) {
-        onClickLoadMore();
-    };
-};
+function loadMorePage() {
+  if (endOfPage()) {
+    onClickLoadMore();
+  }
+}
 
-function checkIfEndOfPage() {
+function endOfPage() {
   return (
     window.innerHeight + window.scrollY >= document.documentElement.scrollHeight
   );
@@ -111,20 +100,18 @@ function clearAll() {
   perPage = 0;
   gallery.innerHTML = ' ';
   loadMoreBtn.classList.add('is-hidden');
-};
+}
 
- 
 function onError() {
-  Notify.failure('Sorry, there are no images matching your search query. Please try again.', {
-    position: 'center-center',
-  });
-};
+  Notify.failure(
+    'Sorry, there are no images matching your search query. Please try again.',
+    {
+      position: 'center-center',
+    }
+  );
+}
 
-
-
-let lightbox = new SimpleLightbox('.photo-wrap a', { 
+let lightbox = new SimpleLightbox('.photo-wrap a', {
   captionsData: 'alt',
   captionDelay: 250,
 });
-
- 
